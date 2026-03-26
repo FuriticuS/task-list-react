f48be70eca20169131e100cf3cfc33d9720c8216
Fur1ouS <Fur1ouS@ya.ru>
2026-03-27 01:51:16 +0300

    add toggle buttons for all blocks and one function for all blocks

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,59 @@ import {Footer} from "./components/footer/Footer.jsx";
 import {useState} from "react";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState({
+    taskList: false,
+    tasks: false,
+    completedTasks: false
+  })
+
+  function toggleOpen(section) {
+    setIsOpen((prev) => ({
+      ...prev,
+      [section]: !prev[section]
+    }));
+  }
 
   return (
     <div className="app">
       <div className="task-container">
         <h1>Task List with Priority</h1>
-        <button className="close-button" onClick={() => setIsOpen(prev => !prev)}>+</button>
-        {isOpen && <TaskForm/>}
+        <button className={`close-button ${isOpen ? 'open' : ''}`} onClick={() => toggleOpen('taskList')}>+</button>
+        {isOpen.taskList && <TaskForm/>}
       </div>
 
       <div className="task-container">
         <h2>Tasks</h2>
-        <button
-          className="close-button">
-          +
-        </button>
+        <button className={`close-button ${isOpen ? 'open' : ''}`} onClick={() => toggleOpen('tasks')}>+</button>
 
-        <div className="sort-controls">
-          <button
-            className={`sort-button`}
-          >
-            By Date
-          </button>
-          <button
-            className={`sort-button`}
-          >
-            By Priority
-          </button>
-        </div>
-          <TaskList />
+        {isOpen.tasks && <>
+          <div className="sort-controls">
+            <button
+              className={`sort-button`}
+            >
+              By Date
+            </button>
+            <button
+              className={`sort-button`}
+            >
+              By Priority
+            </button>
+          </div>
+          <TaskList/>
+        </>}
       </div>
 
       <div className="completed-task-container">
         <h2>Completed Task</h2>
-        <button
-          className={`close-button`}
-        >
-          +
+        <button className={`close-button ${isOpen ? 'open' : ''}`} onClick={() => toggleOpen('completedTasks')}>+
         </button>
-          <CompletedTaskList />
+
+        {isOpen.completedTasks && <CompletedTaskList/>}
       </div>
 
       <Footer/>
     </div>
   );
 }
+
 export default App;
